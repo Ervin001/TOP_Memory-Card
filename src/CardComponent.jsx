0d4c7bf0f486413400bb9cf210bd60c428c06d0f
Ervin Gonzalez <83973082+Ervin001@users.noreply.github.com>
2023-11-18 14:03:40 -0600

    Added API to fetch images, Card Component now shows the image

---
+++ src/CardComponent.jsx
@@ -0,0 +1,17 @@
+import Card from './Card';
+
+export default function CardComponent() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Card type='svg' name='Oreo' />
+      {/* <Card type='svg' name='Gizmo' />
+      <Card type='svg' name='Angel' />
+      <Card type='svg' name='Samantha' />
+      <Card type='svg' name='Lily' />
+      <Card type='svg' name='Baby' />
+      <Card type='svg' name='Sandie' />
+      <Card type='svg' name='Sam' /> */}
+    </>
+  );
+}
